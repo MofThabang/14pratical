d9000f8d5985b981a5ab950d4e685a01006cdbb6
MofThabang <4362668@myuwc.ac.za>
2026-03-04 16:03:32 +0200

    Update chainedHash.java
    inserting class is added

--- chainedHash.java
+++ chainedHash.java
@@ -15,6 +15,85 @@
     private int hash(String key){
     int h = Math.abs(key.hashCode());
       return ( h% m) +1;
+    
+    }
+    public void insert ( String key, String value){
+      int i= hash (key);
+
+  node head + table[i];
+
+  if (head == null){
+    table[i] = new node ( key,value);
+    return;
+  }
+
+  Node current = head;
+  Node prev = null;
+
+    
+
+    prev.next = new Node(key, value);
+}
+    while ( current != null){
+    if ( current.key.equal(key)){
+      current.value = value;
+      return;
+    }
+    prev = current:
+      current = current.next;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
 
 
 
