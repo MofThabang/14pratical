bd97025aa336ec037e17f156f3ca745cd02013e9
MofThabang <4362668@myuwc.ac.za>
2026-03-04 14:29:22 +0200

    Update and rename README.md to openHash.java
    int openHash class

---
+++ openHash.java
@@ -0,0 +1,13 @@
+  public class openHash {
+    private class entry {
+      String key;
+      String value;
+      bolean deleted;
+    }
+
+       Entry(String k, String v) {
+         key =k;
+         value =v;
+         deleted=false;
+       }
+    }
