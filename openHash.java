6d1b1645b48df475374ebc7dd116869704d25869
MofThabang <4362668@myuwc.ac.za>
2026-03-04 14:59:37 +0200

    Update openHash.java
    add insert class

--- openHash.java
+++ openHash.java
@@ -11,3 +11,47 @@
          deleted=false;
        }
     }
+ private Entry[] table;
+    private int m;
+    private int size;
+
+    private int hash(String key) {
+    int h = key.hashCode();
+    h = Math.abs(h);
+    return (h % m) + 1;  // indices [1..m]
+}
+    private int hash(String key) {
+    int h = key.hashCode();
+    h = Math.abs(h);
+    return (h % m) + 1;  // indices [1..m]
+}
+     public void insert (String key, String value){
+        if (isFull()) return;
+
+        int i= hash(key);
+       while (table[i] != null && !table[i].deleted{
+         if (table[i].key.equals(key)){
+           table[i].value= value;
+           return;
+         }
+         i = (i % m) +1;
+       }
+       table[i] = new Entry(key, value);
+       size++;
+     }
+
+    
+
+   
+
+
+
+
+public boolean isEmpty() {
+    return size == 0;
+}
+
+public boolean isInTable(String key) {
+    return lookup(key) != null;
+}
+
