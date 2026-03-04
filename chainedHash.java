65343d64f63c94503f9a06930ee600b54d0faba1
MofThabang <4362668@myuwc.ac.za>
2026-03-04 15:51:02 +0200

    Update chainedHash.java
    private node class and node table are added

--- chainedHash.java
+++ chainedHash.java
@@ -1,1 +1,41 @@
+  public class chainedHash{
+       private class node{
+         String key;
+         String value;
+         Node next;
 
+         Node (String k, Sring v) {
+           key = k;
+           value =v;
+         }
+       }
+    
+    private Node[] table;
+    private int m;
+    private int hash(String key){
+    int h = Math.abs(key.hashCode());
+      return ( h% m) +1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+  
